4595b80a1e2a36062846168976f94c7aac9064b4
Sachin Rajbhar <officialsachinrajbhar@gmail.com>
2025-08-24 01:46:07 +0530

    add error and country details init

--- index.jsx
+++ index.jsx
@@ -1,16 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import Error from './components/Error'
 import Home from './components/Home'
 import Contact from './components/Contact'
 const root = createRoot(document.querySelector('#root'))
 
 // Use React-Router v7 dom with latest Parcel Version Compatibility
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router'
-import Header from './components/Header'
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <Error />,
     children: [
       {
         path: '/',
