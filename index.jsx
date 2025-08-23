88d66e4a9b22845391fa9a4207f486ad505ce270
Sachin Rajbhar <officialsachinrajbhar@gmail.com>
2025-08-23 06:03:38 +0530

    add single-route

--- index.jsx
+++ index.jsx
@@ -1,6 +1,13 @@
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App'
-
 const root = createRoot(document.querySelector('#root'))
 
-root.render(<App />)
+// Use React Router dom
+import { createBrowserRouter, RouterProvider } from 'react-router'
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+]);
+root.render(<RouterProvider router={router} />)
