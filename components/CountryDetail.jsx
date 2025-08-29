23ca07e85220d3888e02844640715fe9eefffbd2
Sachin Rajbhar <officialsachinrajbhar@gmail.com>
2025-08-30 03:03:41 +0530

    pass-data-using-LinkStateAtt-useLocatiionInTargetFile

--- components/CountryDetail.jsx
+++ components/CountryDetail.jsx
@@ -1,54 +1,72 @@
 import React, { useEffect, useState } from 'react'
 import './CountryDetail.css'
-import { Link, useParams } from 'react-router'
+import { Link, useLocation, useParams } from 'react-router'
 export default function CountryDetail() {
   // FETCH STATE
   const params = useParams()
   const countryName = params.country
+  const { state } = useLocation()
+  console.log(state)
 
   const [countryData, setCountryData] = useState(null)
   const [notFound, setNotFound] = useState(false)
+
+  function updateCountryData(data) {
+    setCountryData({
+      name: data.name.common,
+      nativeName: Object.values(data.name.nativeName)[0].common,
+      population: data.population,
+      region: data.region,
+      subregion: data.subregion,
+      capital: data.capital,
+      flag: data.flags.svg,
+      tld: data.tld,
+      currencies: Object.values(data.currencies)
+        .map((currency) => currency.name)
+        .join(', '),
+      languages: Object.values(data.languages).join(', '),
+      borders: [], // Initialize border as empty array
+    })
+
+    if (!data.borders) {
+      data.borders = [] // not gives error & doesn't show if No Borders
+    }
+    Promise.all(
+      // Fetch all border countries promises FOR 3 TIMES RUN ONLY
+      data.borders.map((border) => {
+        return fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+          .then((res) => res.json())
+          .then(([borderCountry]) => borderCountry.name.common)
+      })
+    ).then((borders) => {
+      // to avoid React FastDataOptimization if no borders to stop immediate useLocation hook state update to empty array
+      setTimeout(() =>
+        setCountryData((prevState) => ({ ...prevState, borders }))
+      )
+    })
+  }
+
   useEffect(() => {
+    if (state) {
+      // if state is already FETCHED FROM HOMEPAGE COUNTRIES DATA pass it directly
+      updateCountryData(state)
+      return
+    }
+    // else  fetch data if user TYPES URL directly first time
     fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
       .then((res) => res.json())
       .then(([data]) => {
         // console.log(data);
-        setCountryData({
-          name: data.name.common,
-          nativeName: Object.values(data.name.nativeName)[0].common,
-          population: data.population,
-          region: data.region,
-          subregion: data.subregion,
-          capital: data.capital,
-          flag: data.flags.svg,
-          tld: data.tld,
-          currencies: Object.values(data.currencies)
-            .map((currency) => currency.name)
-            .join(', '),
-          languages: Object.values(data.languages).join(', '),
-          borders: [], // Initialize border as empty array
-        })
-
-        if(!data.borders) {
-          data.borders = [] // not gives error & doesn't show if No Borders
-        }
-        Promise.all( // Fetch all border countries promises FOR 3 TIMES RUN ONLY
-          data.borders.map((border) => {
-            return fetch(`https://restcountries.com/v3.1/alpha/${border}`)
-              .then((res) => res.json())
-              .then(([borderCountry]) => borderCountry.name.common)
-          })
-        ).then((borders) => {
-          setCountryData((prevState) => ({...prevState, borders}))
-        })
-
-      }).catch((err) => { // error if incorrect country name
-        console.log(err);
+        updateCountryData(data)
+      })
+      .catch((err) => {
+        // error if incorrect country name
+        console.log(err)
         setNotFound(true)
       })
   }, [countryName])
 
-  if(notFound) return <div>Country Not Found</div>
+  if (notFound) return <div>Country Not Found</div>
 
   // RENDER
   return countryData === null ? (
@@ -99,12 +117,16 @@ export default function CountryDetail() {
                 <span className="languages"></span>
               </p>
             </div>
-            {countryData.borders.length !== 0 && <div className="border-countries">
-              <b>Border Countries: </b>&nbsp;
-              {
-                countryData.borders.map((border) => <Link key={border} to={`/${border}`}>{border}</Link>)
-              }
-            </div>}
+            {countryData.borders.length !== 0 && (
+              <div className="border-countries">
+                <b>Border Countries: </b>&nbsp;
+                {countryData.borders.map((border) => (
+                  <Link key={border} to={`/${border}`}>
+                    {border}
+                  </Link>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </div>
